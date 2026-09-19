8df584b6a7a9533384b7572b4107e1f34c4d0235
Neskipu <shanessadkhar@gmail.com>
2026-09-20 04:00:46 +0530

    devotional html update

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-app.use(cors());
+app.use(cors({
+    origin: "https://neskipu.github.io"
+}));
+
 app.use(express.json());
 
 
